d2270541facfc8f1af07f883d09de9a22e3c4dd5
miguelrueda <miguel.rueda.e@thalesdigital.io>
2020-09-08 14:33:07 -0500

    Module 5 completed

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
+import { NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-user-settings-form',
   templateUrl: './user-settings-form.component.html',
-  styleUrls: ['./user-settings-form.component.sass']
+  styleUrls: ['./user-settings-form.component.scss']
 })
 export class UserSettingsFormComponent implements OnInit {
+
+  originalUserSettings: UserSettings = {
+    name: '',
+    emailOffers: false,
+    interfaceStyle: '',
+    subscriptionType: '',
+    notes: ''
+  };
+
+  userSettings: UserSettings = { ...this.originalUserSettings };
+
+  result: string;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(`in onSubmit:`, form.valid);
+  }
+
+  onBlur(field: NgModel) {
+    console.log(field.valid);
   }
 
 }
